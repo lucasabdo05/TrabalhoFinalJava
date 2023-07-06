2e77f733c354a7a1a18dd020afbaa7a6acfd3bc1
Lucas Abdo <lucarioales@gmail.com>
2023-07-06 16:51:39 -0300

    Implementação dos métodos para buscar produtos e definir uma quantidade limite

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,11 @@ public class Main {
         System.out.println("Insira o valor do produto: ");
         matriz[linha - 1][2] = scanner.next();
         System.out.println("Produto adicionado.");
-        scanner.close();
+        matriz[linha - 1][3] = Float.toString(Float.parseFloat(matriz[linha - 1][1]) * Float.parseFloat(matriz[linha - 1][2])); // Calculo da coluna total
     }
 
     public static void mostrarProdutos(String[][] matriz, int linhas, int colunas) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Produto | Quantidade | Valor");
+        System.out.println("Produto | Quantidade | Valor | Total");
 
         for (int i = 0; i < linhas; i++)
          {
@@ -35,12 +33,10 @@ public class Main {
                 System.out.print(matriz[i][j] + " | ");
             }
             System.out.println();
-            scanner.close();
         }
     }
 
     public static void calcularValor(String[][] matriz, int linhas) {
-        Scanner scanner = new Scanner(System.in);
         Float valorFinal = 0.0f;
         for (int i = 0; i < linhas; i++) {
             if (matriz[i][0] != null) {
@@ -48,23 +44,118 @@ public class Main {
             }
         }
         System.out.println("Valor final da lista de compras: " + valorFinal + " R$");
-        scanner.close();
+    }
+
+    public static void buscarProduto(String[][] lista, int produtos, String nomeProduto, int quantidadeLimite) {
+        Scanner scanner = new Scanner(System.in);
+        boolean encontrado = false;
+        int opcaoBusca;
+
+        for (int i = 0; i < produtos; i++) {
+            if (lista[i][0] != null && lista[i][0].equals(nomeProduto)) {
+                encontrado = true;
+            }
+        }
+
+        if (encontrado) { // if (encontrado == true) {
+        System.out.println("Produto encontrado.");
+            
+            
+        System.out.println("Escolha uma opção: \n 1 - Atualizar produto. \n 2 - Remover produto \n 0 - Sair.");
+
+        opcaoBusca = scanner.nextInt();
+        switch (opcaoBusca) {
+            case 0:
+                break;
+            case 1:
+                atualizarProduto(lista, produtos, nomeProduto, quantidadeLimite);
+                break;
+            case 2:
+                removerProduto(lista, produtos, nomeProduto);
+                break;
+            default:
+                System.out.println("Opção Inválida!");
+        }
+        } 
+        
+        else {
+            System.out.println("Produto não encontrado.");
+        }
+    }
+
+    public static void atualizarProduto(String[][] matriz, int linhas, String nomeProduto, int quantidadeLimite) {
+        int i;
+
+        for (i = 0; i < linhas; i++) {
+            if (matriz[i][0] != null && matriz[i][0].equals(nomeProduto)) {
+                atualizarNomeDoProduto(matriz, i);
+
+                atualizarQuantidadeDoProduto(matriz, i, quantidadeLimite);
+
+                atualizarValorDoProduto(matriz, i);
+            }
+        }
+        matriz[i - 1][3] = Float.toString(Float.parseFloat(matriz[i - 1][1]) * Float.parseFloat(matriz[i - 1][2])); 
+    }
+
+    public static void atualizarNomeDoProduto(String[][] matriz, int i) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insira o nome do produto atualizado: ");
+        matriz[i][0] = scanner.next();
+    }
+
+    public static void atualizarQuantidadeDoProduto(String[][] matriz, int i, int quantidadeLimite) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Insira a nova quantidade de produtos, ela deve ser menor do que ou igual a " + quantidadeLimite +": ");
+            matriz[i][1] = scanner.next();
+        } while (Integer.parseInt(matriz[i][1]) > quantidadeLimite);
+
+        matriz[i][3] = Float.toString(Float.parseFloat(matriz[i][1]) * Float.parseFloat(matriz[i][2]));
+    }
+
+    public static void atualizarValorDoProduto(String[][] matriz, int i) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insira o valor atualizado do produto: ");
+        matriz[i][2] = scanner.next();
+
+        matriz[i][3] = Float.toString(Float.parseFloat(matriz[i][1]) * Float.parseFloat(matriz[i][2]));
     }
 
     public static void removerProduto(String[][] matriz, int linhas, String nomeProduto) {
-        boolean encontrado = false;
         for (int i = 0; i < linhas; i++) {
             if (matriz[i][0] != null && matriz[i][0].equals(nomeProduto)) {
-                encontrado = true;
                 matriz[i][0] = null;
                 matriz[i][1] = null;
                 matriz[i][2] = null;
+                matriz[i][3] = null;
             }
         }
-        if (encontrado) { // if (encontrado == true) {
-            System.out.println("Produto removido.");
-        } else {
-            System.out.println("Produto não encontrado.");
+        
+        System.out.println("Produto removido.");
+    }
+
+    public static int definirQuantidadeLimite() {
+        Scanner scanner = new Scanner(System.in);
+        int quantidadeLimite;
+
+        System.out.print("Insira a quantidade limite para cada produto: ");
+        quantidadeLimite = scanner.nextInt();
+
+        return quantidadeLimite;
+    }
+
+    public static void verificaSeTodosEstaoDentroDoLimite(String[][] matriz, int linhas, int quantidadeLimite) {
+        int nomeDoProduto = 0;
+        int quantidadeDeProdutos = 1; // O índice da quantidade de um produto é matriz[linhaDoProduto][1]
+
+        for (int i = 0; i < linhas; i++) {
+            if (matriz[i][quantidadeDeProdutos] != null && Integer.parseInt(matriz[i][quantidadeDeProdutos]) > quantidadeLimite) {
+                System.out.println("O produto '" + matriz[i][nomeDoProduto] + "' ultrapassou a quantidade limite.");
+                atualizarQuantidadeDoProduto(matriz, i, quantidadeLimite);
+            }
         }
     }
 
@@ -74,32 +165,43 @@ public class Main {
         int colunas = 4;
         Scanner scanner = new Scanner(System.in);
         String nomeProduto;
+        int quantidadeLimite = 0;
 
-        System.out.println("Insira a quantidade de produtos que serão inseridos na sua lista de compras:");
+        System.out.print("Insira a quantidade de produtos que serão inseridos na sua lista de compras: ");
         produtos = scanner.nextInt();
         lista = new String[produtos][colunas];
 
         do {
-            System.out.println(
-                    "Escolha uma opção: \n 1 - Mostrar lista de compras. \n 2 - Inserir produtos na lista de compras. \n 3 - Calcular valor dos produtos da lista de compras. \n 4 - Remover produto da lista de compras. \n 0 - Sair.");
+            System.out.println("Escolha uma opção: \n 1 - Mostrar lista de compras. \n 2 - Inserir produtos na lista de compras. \n 3 - Calcular valor dos produtos da lista de compras. \n 4 - Buscar produto da lista de compras. \n 5 - Definir quantidade limite. \n 0 - Sair.");
+
             opcao = scanner.nextInt();
             switch (opcao) {
                 case 0:
                     break;
+
                 case 1:
                     mostrarProdutos(lista, produtos, colunas);
                     break;
+
                 case 2:
                     inserirProdutos(lista, produtos, colunas);
                     break;
+                    
                 case 3:
                     calcularValor(lista, produtos);
                     break;
+
                 case 4:
-                    System.out.println("Insira o produto a ser removido.");
+                    System.out.print("Insira o produto a ser buscado: ");
                     nomeProduto = scanner.next();
-                    removerProduto(lista, produtos, nomeProduto);
+                    buscarProduto(lista, produtos, nomeProduto, quantidadeLimite);
                     break;
+
+                case 5:
+                    quantidadeLimite = definirQuantidadeLimite();
+                    verificaSeTodosEstaoDentroDoLimite(lista, produtos, quantidadeLimite);
+                    break;
+
                 default:
                     System.out.println("Opção inválida!");
             }
